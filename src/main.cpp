4ca98246d9cb44d2d67a8e8e60dbd64185f4311b
ORYZAPAO <oryzapao@gmail.com>
2018-07-08 11:35:13 +0900

    SubSysytem CONSOLE -> WINDOWS

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,16 @@
 #include "ofApp.h"
 
 //========================================================================
-int main( ){
+// VisualStudio の 構成プロパティ→リンカ→システム→サブシステム 設定で
+//   Console(/SUBSYSTEM:CONSOLE) --> Windows (/SUBSYSTEM:WINDOWS) に変更して，
+//      コマンドプロンプトが出ないようする．
+//
+//      上記にともない，エントリーポイントが，main() --> WinMain()になるのでその対応
+//
+//========================================================================
+//int main(){
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow){
+
 	ofSetupOpenGL(1024,768,OF_WINDOW);			// <-------- setup the GL context
 
 	// this kicks off the running of my app
